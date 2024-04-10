5c753642338011565b34e04cbaf0db27656aca99
Kayky Belleboni Casagrande <k.belleboni@edu.pucrs.br>
2024-04-09 22:56:48 -0300

    More dotfiles

---
+++ tmux-init.sh
@@ -0,0 +1,16 @@
+folder=$HOME
+folder_name=$(basename "$folder")
+
+if [ -n "$folder" ]; then
+  tmux has-session -t "$folder_name" 2>/dev/null
+
+  if [ $? != 0 ]; then
+    tmux new-session -d -s "$folder_name" -c "$folder"
+  fi
+
+  if [ -z "$TMUX" ]; then
+    tmux attach-session -t "$folder_name"
+  else
+    tmux switch-client -t "$folder_name"
+  fi
+fi
