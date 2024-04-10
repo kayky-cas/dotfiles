a9e2bc20da396d074d11ad368ab6eb51518f34b0
Kayky Belleboni Casagrande <k.belleboni@edu.pucrs.br>
2024-04-09 23:00:42 -0300

    Vim

---
+++ .config/nvim/lua/custom/plugins/harpoon.lua
@@ -0,0 +1,23 @@
+return {
+  'ThePrimeagen/harpoon',
+  branch = 'harpoon2',
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+    local harpoon = require 'harpoon'
+
+    harpoon:setup()
+
+    vim.keymap.set('n', '<leader>m', function()
+      harpoon:list():add()
+    end)
+    vim.keymap.set('n', '<leader>o', function()
+      harpoon.ui:toggle_quick_menu(harpoon:list())
+    end)
+
+    for i = 1, 9, 1 do
+      vim.keymap.set('n', '<leader>' .. i, function()
+        harpoon:list():select(i)
+      end)
+    end
+  end,
+}
