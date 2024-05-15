71789b905c1e932ada9210407b0b13614560b260
Kayky Belleboni Casagrande <kbelleboni@gmail.com>
2024-05-15 17:06:05 -0300

    feat: scripts folder

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -7,6 +7,9 @@ return {
   config = function()
     require('oil').setup {
       skip_confirm_for_simple_edits = true,
+      view_options = {
+        show_hidden = true,
+      },
     }
     vim.keymap.set('n', '<C-n>', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
   end,
